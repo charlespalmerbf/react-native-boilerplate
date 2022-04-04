45e5b1435c12ea2edfec8b87dccce5e0cf5fdc43
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2022-04-04 14:51:39 +0100

    feat: adding react-router-native

---
+++ src/Screens/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {Link, useNavigate} from 'react-router-native';
+
+const LoginScreen = () => {
+
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default LoginScreen;
---
+++ src/Screens/Register.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {Link, useNavigate} from 'react-router-native';
+
+const RegisterScreen = () => {
+
+    return (
+        <View>
+            <Text>Register</Text>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default RegisterScreen;
